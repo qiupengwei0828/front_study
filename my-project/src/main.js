4975cc25906cac87978b308e9dd4673ec72f6a19
仇鹏伟 <1940519031@qq.com>
2020-10-22 15:04:09 +0800

    恢复vue.js相关

--- my-project/src/main.js
+++ my-project/src/main.js
@@ -1,6 +1,23 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-const {app, BrowserWindow} =require('electron');//引入electron
+//引入electron
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
+//引入electron 打开界面，里面仍旧vue
+const {app, BrowserWindow} =require('electron');
+
 let win;
 let windowConfig = {
   width:800,
@@ -27,3 +44,9 @@ app.on('activate',() => {
     createWindow();
   }
 });
+
+
+
+
+
+
